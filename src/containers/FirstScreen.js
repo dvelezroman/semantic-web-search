b04b4dbda8a3f08ddffd6f5202d5cfcadfd23932
Dario Velez Roman <dario.velez@dinmax.com>
2019-12-29 01:24:12 -0300

    web extension config ready

--- src/containers/FirstScreen.js
+++ src/containers/FirstScreen.js
@@ -8,7 +8,8 @@ import ListComponent from '../components/ListComponent';
 
 const useStyles = makeStyles(theme => ({
 	root: {
-		flexGrow: 1
+		flexGrow: 1,
+		width: 600
 	},
 	paper: {
 		height: 140,
