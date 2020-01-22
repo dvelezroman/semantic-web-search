3f562b631c5e3c13cd8ff1b5ba7c1c10588e00f4
Dario Velez Roman <dario.velez@dinmax.com>
2020-01-22 03:15:56 -0300

    fixed from origin and asyncronism in request of DOMs

--- src/resources/sites.js
+++ src/resources/sites.js
@@ -8,10 +8,8 @@ const sites = [
 	{ from: 'origin', name: 'Clarin', url: 'https://www.clarin.com' },
 	{ from: 'origin', name: 'La Nación', url: 'https://www.lanacion.com.ar' },
 	{ from: 'origin', name: 'RT', url: 'https://actualidad.rt.com' },
-	{ from: 'origin', name: 'Primicias', url: 'https://www.primicias.ec' },
 	{ from: 'origin', name: 'El Universo', url: 'https://www.eluniverso.com' },
 	{ from: 'origin', name: 'El Comercio', url: 'https://www.elcomercio.com' },
-	{ from: 'origin', name: 'La Hora', url: 'https://www.lahora.com.ec' },
 	{ from: 'origin', name: 'CNN en Español', url: 'https://cnnespanol.cnn.com' }
 ];
 
--- src/scripts/background/main.js
+++ src/scripts/background/main.js
@@ -3,6 +3,7 @@ import sites from '../../resources/sites';
 
 const instances = [];
 let doms = 0;
+const urlDict = {};
 
 const saveToStorage = (newsInstancesArray, key) => {
 	chrome.storage.local.set(
@@ -27,18 +28,22 @@ const validateUrl = url => {
 	} else return false;
 };
 
+const validateSocialUrl = /^(https?\:\/\/)?((www\.)?youtube\.com|youtu\.?be)\/.+$/;
+
 const initProcess = async site => {
-	if (validateUrl(site.url)) {
+	if (!(site.url in urlDict) && validateUrl(site.url) && !validateSocialUrl.test(site.url)) {
 		try {
-			const DOM = await retrieveDOM(site.url);
+			urlDict[site.url] = true;
+			const { error, data } = await retrieveDOM(site.url);
+			if (error) throw new Error('Error retrieving this site: ' + site.url);
 			doms = doms + 1;
-			const newsInstances = await getUrls(DOM, site);
+			const newsInstances = await getUrls(data, site);
 			instances.push(...newsInstances);
 		} catch (e) {
-			console.log(`Error retrieving: ${site.url}`);
+			console.log(e.message);
 		}
 	} else {
-		console.log('Finished this Loop...');
+		console.log('Url not valid: ' + site.url);
 	}
 };
 
@@ -46,47 +51,59 @@ const retrieveDOM = url =>
 	new Promise((resolve, reject) => {
 		const request = new XMLHttpRequest();
 		request.onload = function(e) {
-			resolve(request.response);
+			if (request.status >= 200 && request.status < 300)
+				resolve({ error: false, data: request.response });
+			else reject({ error: request.statusText, data: null });
 		};
+
 		request.open('GET', url, true);
+
 		request.send();
 	});
 
-const getUrls = (DOM, site) =>
-	new Promise((resolve, reject) => {
-		const $ = cheerio.load(DOM);
-		const config = {
-			selector: 'div > a'
-		};
-		const aTags = $(config.selector).get();
-		const jsonUrls = [];
-		aTags.forEach(tag => {
-			const $tag = $(tag);
-			const href = $tag.attr('href');
-			let url = '';
-			if (href && href.length) {
-				url = validateUrl(href) ? href : `${site.url}${href}`;
+const getUrls = (DOM, site) => {
+	// new Promise((resolve, reject) => {
+	const $ = cheerio.load(DOM);
+	const config = {
+		selector: 'div > a'
+	};
+	const aTags = $(config.selector).get();
+	const jsonUrls = [];
+	aTags.forEach(tag => {
+		const $tag = $(tag);
+		const href = $tag.attr('href');
+		let url = '';
+		if (href && href.length) {
+			if (validateUrl(href)) {
+				url = href;
+			} else if (href[0] === '/') {
+				url = `${site.url}${href}`;
+			} else {
+				url = `${site.url}/${href}`;
 			}
-			const title = $tag.text().trim();
-			jsonUrls.push({ name: site.name, from: site.from, url, title });
-			// TODO: this creates another instance from an URL and control by loop increased in one every time
-		});
-		resolve(jsonUrls);
+		}
+		const title = $tag.text().trim();
+		const urlInstance = { name: site.name, from: site.url, url, title };
+		console.log({ urlInstance });
+		jsonUrls.push(urlInstance);
+		// TODO: this creates another instance from an URL and control by loop increased in one every time
 	});
-
-const sitesPromises = sites.map(site => initProcess(site));
+	return jsonUrls;
+};
 
 const main = async () => {
-	await Promise.all(sitesPromises);
-	console.log('Finished the origin sites Promises');
-	console.log(`Instances: ${instances.length}`);
-	console.log(`Doms obtained: ${doms}`);
-	// now the first loop
-	const childSitesPromises = instances.map(site => initProcess(site));
-	await Promise.all(childSitesPromises);
-	console.log('Finished the second loop of sites Promises');
-	console.log(`Instances: ${instances.length}`);
-	console.log(`Doms obtained: ${doms}`);
+	console.log('Working');
+	for (const site of sites) {
+		await initProcess(site);
+	}
+	console.log('Finish...');
+	console.log('Working');
+	for (const site of instances) {
+		await initProcess(site);
+	}
+	console.log('Finish');
+	console.log({ doms });
+	console.log({ instances });
 };
 
 main();
